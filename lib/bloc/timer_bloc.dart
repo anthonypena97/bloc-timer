da4c38d47ce751a227720c12388020e25cd4ab18
Anthony Pena <apena5@alumni.jh.edu>
2022-07-13 11:36:28 -0400

    created timer state and events + started timer_bloc.dart

---
+++ lib/bloc/timer_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+
+part 'timer_event.dart';
+part 'timer_state.dart';
+
+class TimerBloc extends Bloc<TimerEvent, TimerState>{
+  //TODO: set intial state
+  TimerBloc(): super(){
+//  TODO implement event handlers
+  }
+}
+

8c9d8f085565c357163908828771e82a127b6db1
Anthony Pena <apena5@alumni.jh.edu>
2022-07-14 13:17:23 -0400

    bug: fixed ticker.dart syntax errors

--- lib/ticker.dart
+++ lib/ticker.dart
@@ -1,7 +1,7 @@
-class Ticker{
+class Ticker {
   const Ticker();
-  Stream<int> tick({required int ticks}){
+  Stream<int> tick({required int ticks}) {
     return Stream.periodic(Duration(seconds: 1), (x) => ticks - x - 1)
-      .take(ticks);
-}
+        .take(ticks);
+  }
 }
